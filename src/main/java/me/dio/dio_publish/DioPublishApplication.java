eaa1c0a30c9e2e927763518d2dedeb45c2d81543
Gabriel Cruz dos Passos <passosc.gabriel@hotmail.com>
2024-08-19 14:13:31 -0300

    Initial commit

---
+++ src/main/java/me/dio/dio_publish/DioPublishApplication.java
@@ -0,0 +1,13 @@
+package me.dio.dio_publish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioPublishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioPublishApplication.class, args);
+	}
+
+}
